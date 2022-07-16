635538b66781ddccfb773e16887f585df31b2384
Federico Macías <fma.abogado@gmail.com>
2022-07-16 10:38:54 -0500

    Beginning with  the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import {useEffect,useState} from 'react'
+import './App.css';
 
 const App = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pages, setPages] = useState(0);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/posts?page=${pageNumber}`)
+      .then(res => res.json())
+      .then(({totalPages,posts})=>{
+        console.log(totalPages,posts);
+        setPages(totalPages);
+        setPosts(posts);
+      })
+  },[pageNumber])
+
   return (
-    <div>Hello World</div>
+    <div className="App">
+      <h3>Page of {pageNumber}</h3>
+      {posts.map(
+        post =>(
+          <div key={post._id} className="post">
+            <h4>{post.title}</h4>
+            <p>{post.text}</p>
+            </div>
+        
+        )
+      )}
+    </div>
   )
 }
 
